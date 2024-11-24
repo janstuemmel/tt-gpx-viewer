144cd159dd730b74e38527fb6c232ae7a387560e
Jan <7893682+janstuemmel@users.noreply.github.com>
2024-11-24 23:51:41 +0100

    fix paths

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ map.on("load", () => {
 
 	map.addSource(gpxSourceName, {
 		type: "geojson",
-		data: `gpx://${location.pathname}/24-10-tun.gpx`,
+		data: `gpx://${location.pathname}24-10-tun.gpx`,
 	});
 
 	map.addLayer({
@@ -49,7 +49,7 @@ map.on("load", () => {
 		},
 	});
 
-	fetch(`${location.href}/poi.json`)
+	fetch(`${location.href}poi.json`)
 		.then((res) => res.json())
 		.then((list) =>
 			// biome-ignore lint/complexity/noForEach: <explanation>

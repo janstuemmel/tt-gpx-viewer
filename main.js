e9769eab1c2fbef67ef22e20df86cf1e7fa2b3ab
Jan <7893682+janstuemmel@users.noreply.github.com>
2024-11-25 22:27:12 +0100

    add ruler

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import {length} from '@turf/turf';
 import maplibregl from 'maplibre-gl';
 import * as mapTextProto from 'maplibre-gl-vector-text-protocol';
 import 'maplibre-gl/dist/maplibre-gl.css';
@@ -22,12 +23,16 @@ const map = new maplibregl.Map({
   style: `https://api.maptiler.com/maps/68c69456-9c73-4f0a-82be-e58bad52d250/style.json?key=${apiKey}`,
 });
 
+let rulerActive = false;
+
 map.on('load', async () => {
+  // POI
+
   await map
     .loadImage(`${location.pathname}point.png`)
     .then((res) => map.addImage('poi-marker', res.data));
 
-  fetch(`${location.href}poi.json`)
+  await fetch(`${location.href}poi.json`)
     .then((res) => res.json())
     .then((list) => {
       map.addSource('places', {
@@ -62,9 +67,118 @@ map.on('load', async () => {
         },
       });
     });
-});
 
-map.on('load', () => {
+  // RULER
+
+  const rulerElem = document.getElementById('toggle-ruler');
+  const rulerLenElem = document.getElementById('ruler-len');
+
+  const ruler = {
+    type: 'FeatureCollection',
+    features: [],
+  };
+
+  const rulerLine = {
+    type: 'Feature',
+    geometry: {
+      type: 'LineString',
+      coordinates: [],
+    },
+  };
+
+  rulerElem.addEventListener('click', () => {
+    rulerActive = !rulerActive;
+    if (!rulerActive) {
+      rulerElem.textContent = 'Aktivere Lineal';
+      rulerElem.style.color = 'green';
+      rulerLine.geometry.coordinates = [];
+      ruler.features = [];
+      rulerLenElem.innerText = '0';
+      map.getSource('ruler').setData(ruler);
+    } else {
+      rulerElem.textContent = 'Deaktivere Lineal';
+      rulerElem.style.color = 'chocolate';
+    }
+  });
+
+  map.addSource('ruler', {type: 'geojson', data: ruler});
+  map.addLayer({
+    id: 'ruler-points',
+    type: 'circle',
+    source: 'ruler',
+    paint: {
+      'circle-radius': 8,
+      'circle-color': '#000',
+      'circle-opacity': 0.6,
+    },
+    filter: ['in', '$type', 'Point'],
+  });
+  map.addLayer({
+    id: 'ruler-lines',
+    type: 'line',
+    source: 'ruler',
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round',
+    },
+    paint: {
+      'line-color': '#000',
+      'line-width': 4,
+      'line-opacity': 0.6,
+    },
+    filter: ['in', '$type', 'LineString'],
+  });
+
+  map.on('click', (e) => {
+    if (!rulerActive) {
+      return;
+    }
+
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ['ruler-points'],
+    });
+
+    if (ruler.features.length > 1) ruler.features.pop();
+
+    rulerLenElem.innerText = '0';
+
+    if (features.length) {
+      const id = features[0].properties.id;
+      ruler.features = ruler.features.filter((p) => p.properties.id !== id);
+    } else {
+      ruler.features.push({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [e.lngLat.lng, e.lngLat.lat],
+        },
+        properties: {
+          id: String(new Date().getTime()),
+        },
+      });
+    }
+
+    if (ruler.features.length > 1) {
+      rulerLine.geometry.coordinates = ruler.features.map(
+        (p) => p.geometry.coordinates,
+      );
+      ruler.features.push(rulerLine);
+      const len = length(rulerLine).toLocaleString();
+      rulerLenElem.innerText = len;
+    }
+
+    map.getSource('ruler').setData(ruler);
+  });
+
+  map.on('mousemove', (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ['ruler-points'],
+    });
+    map.getCanvas().style.cursor = features.length ? 'pointer' : 'crosshair';
+  });
+
+  // GPX
+
   const filesElem = document.getElementById('files');
   ROUTES.forEach(({file, name, color}) => {
     map.addSource(name, {
